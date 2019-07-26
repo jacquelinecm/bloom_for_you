758bc4b838f4be4ecedc5fa8435361b8c3013f72
GWC <fmcnamara21@siprep.org>
2019-07-26 13:35:34 -0700

    Add hello.py to repository

---
+++ hello.py
@@ -0,0 +1,24 @@
+# print("Hello, World!")
+# answer = input("Who inspires you?")
+# print (answer, "inspires you!")
+# while True:
+#     print ("forever looping!")
+# while looping
+# i = 0
+# while i < 5 :
+#     print (i)
+#     i += 1
+# for i in range (1,5,2) :
+#     print (i)
+i = -1
+while True:
+    i += 1
+
+    if(i > 20):
+        break
+
+        # i is odd
+    if(i % 2 != 0):
+        continue
+
+    print(i)
